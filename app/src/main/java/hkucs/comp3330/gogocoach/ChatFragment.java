ac52ed83b0b9814153e2bf66c1078329694fe75c
Eric Lau <ericlauchiho@gmail.com>
2019-05-14 18:20:15 +0800

    fix chat icon

--- app/src/main/java/hkucs/comp3330/gogocoach/ChatFragment.java
+++ app/src/main/java/hkucs/comp3330/gogocoach/ChatFragment.java
@@ -1,6 +1,13 @@
 package hkucs.comp3330.gogocoach;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
@@ -32,6 +39,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -209,13 +218,26 @@ public class ChatFragment extends Fragment {
                     viewHolder.messengerImageView.setImageDrawable(ContextCompat.getDrawable(getActivity(),
                             R.drawable.ic_account_circle_black_36dp));
                 } else {
-                    Glide.with(ChatFragment.this)
-                            .load(message.getPhotoUrl())
-                            .into(viewHolder.messengerImageView);
+                    final MessageViewHolder _holder = viewHolder;
+                    final Message _message = message;
+                    (new Thread(new Runnable(){
+                        @Override
+                        public void run() {
+                            final Bitmap avatar = getCroppedBitmap(loadImageFromNetwork(_message.getPhotoUrl()));
+                            _holder.messengerImageView.post(new Runnable(){
+                                @Override
+                                public void run() {
+                                    _holder.messengerImageView.setImageBitmap(avatar);
+                                }
+                            });
+                        }
+                    })).start();
                 }
 
             }
         };
+
+
 
         mFirebaseAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
             @Override
@@ -290,6 +312,42 @@ public class ChatFragment extends Fragment {
         });
         return view;
     }
+
+    public Bitmap loadImageFromNetwork(String url) {
+        try {
+            Bitmap bitmap = BitmapFactory.decodeStream
+                    ((InputStream) new URL(url).getContent());
+            return bitmap;
+        } catch(
+                Exception e)
+
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public Bitmap getCroppedBitmap(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        // canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
+                bitmap.getWidth() / 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        //Bitmap _bmp = Bitmap.createScaledBitmap(output, 60, 60, false);
+        //return _bmp;
+        return output;
+    }
+
     @Override
     public void onPause() {
         mFirebaseAdapter.stopListening();
